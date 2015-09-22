37c92103579a631fb191978d098f293c3085d188
ellipse42&Cafe <ellipse43@gmail.com>
2015-09-22 15:28:00 +0800

    add Menu xib

--- v2ex/MenuViewController.swift
+++ v2ex/MenuViewController.swift
@@ -2,15 +2,22 @@
 //  MenuViewController.swift
 //  v2ex
 //
-//  Created by ellipse42 on 15/9/3.
-//  Copyright (c) 2015年 ellipse42. All rights reserved.
+//  Created by ellipse42 on 15/9/22.
+//  Copyright © 2015年 ellipse42. All rights reserved.
 //
 
 import UIKit
 
 class MenuViewController: UIViewController {
+    
+    override func loadView() {
+        NSBundle.mainBundle().loadNibNamed("MenuViewController", owner: self, options: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+
 }
